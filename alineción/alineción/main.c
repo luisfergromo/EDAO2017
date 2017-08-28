d3194268a7772ef4c372cc5944bc2d3256573b95
luisromo <changoluis@hotmail.com>
2017-08-28 12:50:03 -0500

    asdf

--- alineción/alineción/main.c
+++ alineción/alineción/main.c
@@ -8,17 +8,35 @@
 
 #include <stdio.h>
 struct Date{
-    short day,month,year;// 
-    char name[10];
+    short day,month,year;//12
+    char name[10];//3
 };
 struct Ticket{
-    unsigned int idCompra;
-    double costo;
-    struct Date date;
+    unsigned int idCompra; //4
+    double costo;//8
+    struct Date date; //18
+};
+struct struct1{
+    char c1,c2;
+    short s1;
+    int i;
+};
+struct struct2{
+    char c1;
+    short s1;
+    char  c2;
+    int i;
+};
+struct strcut3{
+    short s1;
+    char c1;
 };
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("%d\n",sizeof(struct Date));
     printf("%d\n",sizeof(struct Ticket));
+    printf("%d\n",sizeof(struct struct1)); //8bytes
+    printf("%d\n",sizeof(struct struct2)); //12bytes
+    printf("%d\n",sizeof(struct strcut3)); //4bytes
     return 0;
 }
