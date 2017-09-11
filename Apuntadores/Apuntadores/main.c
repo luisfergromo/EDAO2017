f26a4670423bc65e26cbe0be0e33f89fe11c3ab4
luisromo <changoluis@hotmail.com>
2017-09-11 11:45:06 -0500

    ptr

--- Apuntadores/Apuntadores/main.c
+++ Apuntadores/Apuntadores/main.c
@@ -9,7 +9,17 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a=5;
+    int *b;
+    int c=10;
+    b =&a;
+    printf("               Dir de (a): %p\n",&a);
+    printf("             Valor de (a): %d\n",a);
+    printf("               Dir de (b): %p\n",&b);
+    printf("  Dir de donde apunta (b): %p\n",b);
+    printf("Valor de donde apunta (b): %d\n",*b);
+    b=&c;
+    printf("  Dir de donde apunta (b): %p\n",b);
+    printf("Valor de donde apunta (b): %d\n",*b);
     return 0;
 }
