b8044d9e272c240f267bb6bba6af42feb2584cbc
luisromo <changoluis@hotmail.com>
2017-09-13 11:28:48 -0500

    asdf

---
+++ funciones/funciones/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  funciones
+//
+//  Created by Luis Romo on 13/09/17.
+//  Copyright © 2017 Luis Romo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
