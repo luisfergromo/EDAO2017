3407788f9d0027ab3da5a9362679ce8ea88ee8e0
luisromo <changoluis@hotmail.com>
2017-09-13 12:03:09 -0500

    asdfd

--- funciones/funciones/main.c
+++ funciones/funciones/main.c
@@ -8,8 +8,41 @@
 
 #include <stdio.h>
 
+
+    //declaración de función
+/*
+ int duplicar(int a);
+ void duplicar2(int *b);
+ */
+void promedio(int longitud, float *valor);
+
+void duplicar(int a){ //Pase por valor
+     a*=2;
+}
+int duplicar2(int *b){//Pase por referencia
+    return *b=(*b)*2;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n=2;
+    float array[]={1,2,3,4,5,6,7,8,9};
+    promedio(9,array); //no usar sizeof(array)
+
+        //printf("%d\n",duplicar(2));
+        //printf("%d\n",duplicar2(&n));
     return 0;
+}
+void promedio(int longitud,float *valores){ //Es valido: void promedio(int longitud,float valores[]){ o float valores[5]
+    float suma = 0.0;
+    int i;
+    for (i=0; i<longitud; i++) {
+
+        suma+=valores[i];
+        /* Es valido hacer:
+        suma+=*(valores+1);
+                        o
+        suma+=(*valores);
+        valores++;
+         */
+    }
+    printf("%.2f\n",suma/longitud);
 }
