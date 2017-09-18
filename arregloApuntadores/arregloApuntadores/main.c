1a1198b63aaff569a5e2e4654ad0858a78c7a9db
luisromo <changoluis@hotmail.com>
2017-09-18 13:20:53 -0500

    asdfg

--- arregloApuntadores/arregloApuntadores/main.c
+++ arregloApuntadores/arregloApuntadores/main.c
@@ -7,9 +7,22 @@
 //
 
 #include <stdio.h>
+void filtroNombres(char* nombres[5],char letra);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char *nombres[4] ={"Andres","Pablo","Raúl","Luis"};
+/*
+ int a=10, b=20,c=30;
+ int *p[3]={&a,&b,&c};
+ printf("Contenido de p[0]: %d\n", *p[0]);
+ printf("Contenido de p[1]: %d\n", *p[1]);
+ printf("Contenido de p[2]: %d\n", *p[2]);
+*/
+    filtroNombres(nombres, "A");
     return 0;
+}
+void filtroNombres(char* nombres[5],char letra){
+    for (int i=0; i<5; i++) {
+
+    }
 }
